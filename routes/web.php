d20c32f95019e6fe946e8bb1f38c25faa62e118c
GustavoSMelo <gsantos15569@gmail.com>
2026-07-24 02:57:54 -0300

    update

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,5 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::livewire('/', 'app::welcome')->name('welcome');
-Route::livewire('/prayer/result', 'app::prayer-result')->name('prayer.result');
+Route::livewire("/", "app::welcome")->name("welcome");
+Route::livewire("/prayer/result", "app::prayer-result")->name("prayer.result");
